572c64e092871c87d31164a02d1ef254c0eb5ab7
codewithmawais <connectwithmawais>
2023-07-25 04:10:36 +0500

    Setup a server BoilerPlate.

---
+++ server/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+const app = express();
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import commentRoutes from "./routes/comments.js";
+import likeRoutes from "./routes/likes.js";
+import relationshipRoutes from "./routes/relationships.js";
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/likes", likeRoutes);
+app.use("/api/relationships", relationshipRoutes);
+
+app.listen(8800, () => {
+  console.log("API working!");
+});
