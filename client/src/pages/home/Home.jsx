9c9ccdc64e293f0bf7564261c69134bb230a0a50
codewithmawais <connectwithmawais>
2023-07-24 19:58:21 +0500

    Implemented Share feature UI.

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import Stories from "../../components/stories/Stories";
+import Share from "../../components/share/Share"
 import Posts from "../../components/posts/Posts";
 import "./home.scss"
 
 const Home = () => {
   return (
     <div className="home">
-      <Stories/>
+      <Stories />
+      <Share/>
       <Posts/>
     </div>
   )
