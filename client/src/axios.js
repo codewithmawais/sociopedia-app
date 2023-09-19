6d767850089231eaa6358bb0f0dd377b9a99b32f
codewithmawais <connectwithmawais>
2023-09-19 12:43:09 +0500

    Update axios file.

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const makeRequest = axios.create({
-    baseURL: "http://localhost:8800/api/",
+    baseURL: process.env.REACT_APP_BACKEND_URL,
     withCredentials: true,
 });
