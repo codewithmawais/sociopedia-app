6b4080563f861472897b9847b774c20c96725a27
codewithmawais <connectwithmawais>
2023-10-08 20:45:02 +0500

    Update require package.

--- server/connect.js
+++ server/connect.js
@@ -1,6 +1,6 @@
 import 'dotenv/config'
-import mysql from "mysql"
+import mysql2 from "mysql2"
 
 const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}`
 
-export const db = mysql.createConnection(urlDB);
+export const db = mysql2.createConnection(urlDB);
