bc4a6515e62a90b248b67fb841b3366eaa6c6c57
codewithmawais <connectwithmawais>
2023-10-08 16:11:05 +0500

    Update cors orign. Allowed access to all origins.

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://simple-sociopedia.netlify.app/",
+    origin: "*",
   })
 );
 app.use(cookieParser());
