d8bbb422ef59680ede9998db85d59a65c4efd9b5
codewithmawais <connectwithmawais>
2023-10-08 15:50:02 +0500

    Update cors orign.

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://simple-sociopedia.netlify.app/",
   })
 );
 app.use(cookieParser());
