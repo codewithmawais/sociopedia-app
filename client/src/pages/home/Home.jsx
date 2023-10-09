9f946360461deb1a50df12368cac3192d21fb100
codewithmawais <connectwithmawais>
2023-10-09 14:40:33 +0500

    Fixed userId prop in HomePage component.

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,14 +1,17 @@
 import Stories from "../../components/stories/Stories";
 import Share from "../../components/share/Share"
 import Posts from "../../components/posts/Posts";
+import { useContext } from "react";
+import { AuthContext } from "context/authContext";
 import "./home.scss"
 
 const Home = () => {
+  const { currentUser } = useContext(AuthContext);
   return (
     <div className="home">
       <Stories />
       <Share/>
-      <Posts/>
+      <Posts userId={currentUser.id}/>
     </div>
   )
 }
